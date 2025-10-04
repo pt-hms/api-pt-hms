93015c88d6798c26a210f0c77ff1304e96e7b4e7
Badzlan Nur Dhabith <badzlandhabith05@gmail.com>
2025-10-05 02:29:30 +0700

    get all drivers feature

--- src/controllers/drivers.controller.js
+++ src/controllers/drivers.controller.js
@@ -1,7 +1,37 @@
 import bcrypt from "bcrypt";
 import prisma from "../../prisma/client.js";
 
-export const createDriver = async (req, res) => {};
+export const createDriver = async (req, res) => {
+   const { nama, password, no_hp, no_pol, kategori, foto_profil } = req.body;
+
+   if (!nama || !password || !no_hp || !no_pol || !kategori || !foto_profil) {
+      return res.status(400).json({ message: "Semua field harus diisi" });
+   }
+
+   const exist = await prisma.driver.findUnique({
+      where: { no_hp },
+   });
+
+   if (exist) {
+      return res.status(400).json({ message: "Nomor HP sudah terdaftar" });
+   }
+
+   const hashedPassword = await bcrypt.hash(password, 10);
+
+   const driver = await prisma.driver.create({
+      data: {
+         nama,
+         password: hashedPassword,
+         no_hp,
+         no_pol,
+         kategori,
+         foto_profil,
+         role: "driver",
+      },
+   });
+
+   return res.status(201).json({ message: "Driver berhasil dibuat", driver });
+};
 
 export const getAllDrivers = async (req, res) => {
    const { kategori } = req.query;
