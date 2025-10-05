074bf3599a8380cddd30d1184670a4d8914a89ef
Badzlan Nur Dhabith <badzlandhabith05@gmail.com>
2025-10-05 14:11:09 +0700

    update order feature

--- src/controllers/orders.controller.js
+++ src/controllers/orders.controller.js
@@ -12,7 +12,36 @@ export const getOrderById = async (req, res) => {
    return res.status(200).json({ order });
 };
 
-export const updateOrder = async (req, res) => {};
+export const updateOrder = async (req, res) => {
+   const { id } = req.params;
+   const { nama_driver, no_hp, no_pol, kategori, pickup_point, tujuan } = req.body;
+
+   if (!nama_driver || !no_hp || !no_pol || !kategori || !pickup_point || !tujuan) {
+      return res.status(400).json({ message: "Semua field harus diisi" });
+   }
+
+   const order = await prisma.order.findUnique({
+      where: { id: Number(id) },
+   });
+
+   if (!order) {
+      return res.status(400).json({ message: "Order tidak ditemukan" });
+   }
+
+   await prisma.order.update({
+      where: { id: Number(id) },
+      data: {
+         nama_driver,
+         no_hp,
+         no_pol,
+         kategori,
+         pickup_point,
+         tujuan,
+      },
+   });
+
+   return res.status(200).json({ message: "Order berhasil diperbarui" });
+};
 
 export const deleteOrder = async (req, res) => {
    const { id } = req.params;
