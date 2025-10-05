8952133d85fc873760720cc4d277179743aeb401
Badzlan Nur Dhabith <badzlandhabith05@gmail.com>
2025-10-05 15:10:00 +0700

    create order via ocr feature

--- src/controllers/orders.controller.js
+++ src/controllers/orders.controller.js
@@ -1,4 +1,48 @@
-export const createOrder = async (req, res) => {};
+import prisma from "../../prisma/client.js";
+import { createWorker } from "tesseract.js";
+
+export const createOrder = async (req, res) => {
+   const { gambar } = req.body;
+
+   if (!gambar || !gambar.startsWith("data:image")) {
+      return res.status(400).json({ message: "Image tidak valid" });
+   }
+
+   const worker = await createWorker("eng");
+   const data = await worker.recognize(gambar);
+
+   const pickupOptions = ["1A", "1B", "1C", "2D", "2E", "2F", "3 Domestik", "3 Internasional"];
+   let pickup = pickupOptions.find((opt) => data.data.text.toLowerCase().includes(opt.toLowerCase()));
+   if (pickup) {
+      if (!pickup.toLowerCase().startsWith("terminal")) {
+         pickup = `Terminal ${pickup}`;
+      }
+   } else {
+      pickup = "Pick up point Tidak ditemukan";
+   }
+
+   const tujuanMatch = data.data.text.match(/Menurunkan([\s\S]*?)Penumpang/i);
+   let tujuan = null;
+   if (tujuanMatch) {
+      tujuan = tujuanMatch[1].replace(/\n+/g, " ").trim();
+   } else {
+      tujuan = "Tujuan Tidak ditemukan";
+   }
+
+   const order = await prisma.order.create({
+      data: {
+         nama_driver: req.user.nama,
+         no_hp: req.user.no_hp,
+         no_pol: req.user.no_pol,
+         kategori: req.user.kategori,
+         pickup_point: pickup,
+         tujuan,
+         driverId: req.user.id,
+      },
+   });
+
+   return res.status(201).json({ message: "Order berhasil dibuat", order });
+};
 
 export const getAllOrders = async (req, res) => {};
 
